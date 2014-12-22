8551617a2372d24a4925daa06d7de72e02dfce79
Aidan Steele <aidan.steele@glassechidna.com.au>
2014-12-22 15:49:47 +1100

    No need for dup

--- lib/stackprofiler/filters/compress_tree.rb
+++ lib/stackprofiler/filters/compress_tree.rb
@@ -8,8 +8,8 @@ module Stackprofiler
       def filter root, frames
         root.reverse_depth_first do |node|
           if node.out_degree == 1
-            hash = node.content.dup
-            hash[:addrs] = hash[:addrs] + node.first_child.content[:addrs]
+            hash = node.content
+            hash[:addrs] += node.first_child.content[:addrs]
             repl = Tree::TreeNode.new(node.name, hash)
             node.first_child.children.each {|n| repl << n }
             node.replace_with repl
