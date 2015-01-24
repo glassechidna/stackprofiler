013ad4685eb4ebfa782d67469fdd6864e351bf7d
Aidan Steele <aidan.steele@glassechidna.com.au>
2015-01-25 10:39:06 +1100

    Add todo re: gem removal filter

--- lib/stackprofiler/filters/gem_removal.rb
+++ lib/stackprofiler/filters/gem_removal.rb
@@ -8,6 +8,9 @@ module Stackprofiler
 
       def filter root, run
         remove_frames root, run do |node, frame|
+          # todo: gem determination depends on gems being
+          # run from the same interpreter/rubygems path
+          # as stackprofiler. is that a good idea?
           Gem.path.any? {|p| frame[:file].include?(p) }
         end
       end
